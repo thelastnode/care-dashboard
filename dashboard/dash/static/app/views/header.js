7b2ebb994f63cc66de3374c1b74cdc98ab2f8f1b
Ankit Shankar <ankitshankar@gmail.com>
2012-04-02 21:41:42 -0400

    Add ability to log out

---
+++ dashboard/dash/static/app/views/header.js
@@ -0,0 +1,24 @@
+$(function() {
+  "use strict";
+
+  window.Views = window.Views || {};
+
+  window.Views.Header = Backbone.View.extend({
+    template: _.template($('#tmpl-header').text()),
+
+    events: {
+      'click .logout-button': 'logout',
+    },
+
+    render: function() {
+      this.$el.html(this.template({
+        name: window.user.get('first_name') + ' '
+          + window.user.get('last_name'),
+      }));
+    },
+
+    logout: function() {
+      this.trigger('logout');
+    },
+  });
+});
